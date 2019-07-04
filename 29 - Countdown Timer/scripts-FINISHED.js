38ef96b466cacf3b138d0891aeb3313ed193a089
Brent Johnson <brjohnson@youniqueproducts.com>
2019-07-03 22:18:13 -0600

    Adding day 29

--- 29 - Countdown Timer/scripts-FINISHED.js
+++ 29 - Countdown Timer/scripts-FINISHED.js
@@ -35,7 +35,7 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const adjustedHour = hour > 12 ? (hour - 12) : hour;
   const minutes = end.getMinutes();
   endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
